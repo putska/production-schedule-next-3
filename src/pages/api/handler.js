1d28a8f950105fea8b43f34627d18cea35c5e6a6
putska <stevedwatts@gmail.com>
2023-09-05 16:43:37 -0700

    using handler

--- src/pages/api/handler.js
+++ src/pages/api/handler.js
@@ -23,21 +23,26 @@ export default async function handler(req, res) {
   try {
     // const paths = await api.pathsToRevalidate()
     // console.log(paths)
+
     // // Revalidate every path
     // await Promise.all(paths.map(res.revalidate))
+
     // for (let path of paths) {
     //   try {
     //     await res.revalidate(path)
     //   } catch (error) {
     //     console.log(error)
     //   }
+
     // }
-    //await res.revalidate("/")
+
+    await res.revalidate("/");
+
     // Return a response to confirm everything went ok
-    //return res.json({ revalidated: true })
+    return res.json({ revalidated: true });
   } catch (err) {
     // If there was an error, Next.js will continue
     // to show the last successfully generated page
-    //return res.status(500).send('Error revalidating: ' + err)
+    return res.status(500).send("Error revalidating: " + err);
   }
 }
